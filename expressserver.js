10533a47ad9a949fce7bfa524b8f2ada3c32c229
RamavathyNidamanuri <ramacse9@gmail.com>
2022-01-23 13:46:16 -0500

    Create expressserver.js

---
+++ expressserver.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+
+app.get('/', function (req, res) {
+    res.send(' first express http server');
+});
+
+app.get('/Helloworld', function (req, res) {
+    res.send('Hello world with express server');
+});
+
+app.use(function(req, res, next) {
+    res.status(404).send("404 error :)");
+});
+
+app.listen(3000, function () {
+    console.log(' listening on port 3000.');
+});
