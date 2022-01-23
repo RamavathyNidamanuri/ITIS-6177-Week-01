34b1162e3cbd6a18b8788fc3383a0f9e72ff9c82
RamavathyNidamanuri <ramacse9@gmail.com>
2022-01-23 13:23:20 -0500

    Create nodeserver.js

---
+++ nodeserver.js
@@ -0,0 +1,9 @@
+var http = require('http'); 
+
+var server = http.createServer(function (req, res) {   
+
+});
+
+server.listen(3000); 
+
+console.log('Node.js web server at port 3000 is running..')
