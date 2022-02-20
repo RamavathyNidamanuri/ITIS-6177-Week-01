e007deb8b6d73e6b65ac6abadd4389bc414867bf
RamavathyNidamanuri <ramacse9@gmail.com>
2022-02-20 13:21:20 -0500

    Update nodeserver.js

--- nodeserver.js
+++ nodeserver.js
@@ -1,14 +1,24 @@
 const http = require('http');
-
 const hostname = 'localhost';
-const port = 8080;
+const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World!!!');
+  const { exec } = require("child_process");
+  exec("start .", (error, stdout, stderr) => {
+   if (error) {
+       console.log(`error: ${error.message}`);
+       return;
+   }
+   if (stderr) {
+       console.log(`stderr: ${stderr}`);
+       return;
+   }
+   console.log(`stdout: ${stdout}`);
 });
-
+});
 server.listen(port, hostname, () => {
-  console.log(`Server is running`);
+  console.log(`Server is running on port`, port);
 });
